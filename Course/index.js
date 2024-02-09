120b284377715975e0c91a62f62cf7437bea8bb3
Sengeki1 <gamersengeki@gmail.com>
2024-02-09 18:37:37 -0100

    wrapping up containers

--- Course/index.js
+++ Course/index.js
@@ -1,10 +1,14 @@
 const h1 = document.createElement("h1")
 h1.textContent = "This is an imperative way to program"
 h1.className = "header"
-console.log(h1)
-
-const element = <h1 className="header">This is JSX</h1>
-console.log(element)
+//document.getElementById("root").append(h1)
 
 //JSX
-ReactDOM.render(element, document.getElementById("root"))
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
