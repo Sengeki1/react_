4c496fa36ded7892b615bf75fbab83f6d4d151d6
Sengeki1 <gamersengeki@gmail.com>
2024-02-09 18:33:36 -0100

    JSX

--- Course/index.js
+++ Course/index.js
@@ -1,4 +1,10 @@
 const h1 = document.createElement("h1")
 h1.textContent = "This is an imperative way to program"
 h1.className = "header"
-document.getElementById("root").append(h1)
+console.log(h1)
+
+const element = <h1 className="header">This is JSX</h1>
+console.log(element)
+
+//JSX
+ReactDOM.render(element, document.getElementById("root"))
