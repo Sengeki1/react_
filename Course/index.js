42b2604dd87bd57332136743015134329da69883
Sengeki1 <gamersengeki@gmail.com>
2024-02-09 18:09:31 -0100

    personalized containers

--- Course/index.js
+++ Course/index.js
@@ -1,6 +1,21 @@
+function MainContent() {
+    return (
+        <h1>I'm Learning React!</h1>
+    )
+}
+
+function Navbar() {
+    return (
+        <nav>
+            <a href="/html/">HTML</a> |
+            <a href="/css/">CSS</a> |
+            <a href="/js/">JavaScript</a> |
+            <a href="/python/">Python</a>
+        </nav>
+    )
+}
+
 ReactDOM.render(<div id="class1">
-    <ul>
-        <li> Marco</li>
-        <li> Soares</li>
-    </ul>
+    <Navbar />
+    <MainContent />
 </div>, document.getElementById("root"))
