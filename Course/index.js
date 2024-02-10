41ffd1c821ddf87d20749066236f5f5e090e948c
Sengeki1 <gamersengeki@gmail.com>
2024-02-10 08:44:51 -0100

    JSX

--- Course/index.js
+++ Course/index.js
@@ -1,14 +1,12 @@
-const h1 = document.createElement("h1")
-h1.textContent = "This is an imperative way to program"
-h1.className = "header"
-//document.getElementById("root").append(h1)
-
-//JSX
-const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
-    </div>
+const navbar = (
+    <nav>
+        <h1>Sengeki</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(navbar, document.getElementById("root"))
