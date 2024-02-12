836e3323730c70334703970e39929834c642aa42
Sengeki1 <gamersengeki@gmail.com>
2024-02-12 14:14:57 -0100

    Components JSX

---
+++ my-app/src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+    return (
+        <footer>
+            <p>© 2023 Soares development. All rights reserved</p>
+        </footer>
+    )
+}
---
+++ my-app/src/Components/MainContent/MainContent.jsx
@@ -0,0 +1,12 @@
+export default function MainContent () {
+    return (
+        <div>
+            <h1>I'm excited to learn React</h1>
+            <ol className="list">
+                <li>I like learning new things</li>
+                <li>It's best fit to learn react for a job
+                as a developer</li>
+            </ol>
+        </div>
+    )
+}
