1654cc99297b6d721632a39f067596f19e143136
Sengeki1 <gamersengeki@gmail.com>
2024-02-14 16:23:11 -0100

    About section

--- digital_business_card/src/App.js
+++ digital_business_card/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import './App.css'
+import Info from './Components/Info'
+import About from './Components/About'
+import Interests from './Components/Interests'
+import Footer from './Components/Footer'
 
 function App() {
   return (
-    <div>
-      
+    <div className="main">
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
     </div>
   );
 }
