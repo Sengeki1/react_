927604ca7a50a441e66c82828de345f19bc50508
Sengeki1 <gamersengeki@gmail.com>
2024-02-14 16:38:09 -0100

    Info section

--- digital_business_card/src/Components/Interests.jsx
+++ digital_business_card/src/Components/Interests.jsx
@@ -1,7 +1,10 @@
 export default function Interests () {
     return (
         <div>
-            
+            <h2>Interests</h2>
+            <p>
+                i like computers and making video games
+            </p>
         </div>
     )
 }
