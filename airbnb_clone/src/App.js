0b1658e4e35997684fd296a666fc64f9db0e2685
Sengeki1 <gamersengeki@gmail.com>
2024-02-25 11:56:32 -0100

    Hero Section

--- airbnb_clone/src/App.js
+++ airbnb_clone/src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './Components/Header/NavBar'
+import Hero from './Components/Body/Hero'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ airbnb_clone/src/Components/Body/Hero.jsx
@@ -0,0 +1,13 @@
+import './Body.css'
+
+export default function Hero () {
+    return (
+        <div>
+            <img src='./Images/Group 77.png' className='hero--images'/>
+            <h1>Online Experiences</h1>
+            <p>
+                Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+        </div>
+    )
+}
