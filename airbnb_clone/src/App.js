dd9afdba9b0457646dda4cb9e4c78fe889a824bb
Sengeki1 <gamersengeki@gmail.com>
2024-02-25 13:01:16 -0100

    Card section

--- airbnb_clone/src/App.js
+++ airbnb_clone/src/App.js
@@ -1,5 +1,6 @@
 import NavBar from './Components/Header/NavBar'
 import Hero from './Components/Body/Hero'
+import Card from './Components/Card/Card'
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Hero />
+      <Card />
     </div>
   );
 }
---
+++ airbnb_clone/src/Components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import './Card.css'
+
+export default function Card () {
+    return (
+        <div className='card'>
+            <img src='./Images/Card/image 12.png' className="photo"/><br/>
+            <div className='details'>
+                <img src='./Images/Card/Star 1.png'className="icon"/>
+                <a>5.0 <span>(6) - USA</span></a>
+            </div>
+            <p className='text-1'>Life lessons with Katie Zaferes</p>
+            <p className='text-2'><b>From $136</b> / person</p>
+        </div>
+    )
+}
