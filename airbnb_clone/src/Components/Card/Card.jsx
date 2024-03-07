ca6d920b19751d88952275bffee4e3ce3cc5d723
Sengeki1 <gamersengeki@gmail.com>
2024-03-07 15:18:21 -0100

    card component (hard-code data)

--- airbnb_clone/src/Components/Card/Card.jsx
+++ airbnb_clone/src/Components/Card/Card.jsx
@@ -3,7 +3,10 @@ import './Card.css'
 export default function Card () {
     return (
         <div className='card'>
-            <img src='./Images/Card/image 12.png' className="photo"/><br/>
+            <div className='profile'>
+                <p>SOLD  OUT</p>
+                <img src='./Images/Card/image 12.png' className="photo"/><br/>
+            </div>
             <div className='details'>
                 <img src='./Images/Card/Star 1.png'className="icon"/>
                 <a>5.0 <span>(6) - USA</span></a>
