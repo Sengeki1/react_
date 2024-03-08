25e73dedbadb4060c51dca0fbad684ba1e462739
Sengeki1 <gamersengeki@gmail.com>
2024-03-08 17:54:31 -0100

    contact card divs

--- props/src/App.js
+++ props/src/App.js
@@ -1,19 +1,11 @@
+import Contact from './Components/Contact.jsx'
 import './App.css';
 
 function App() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-
-    if (hours < 12) {
-      timeOfDay = "Morning"
-    } else if (hours > 12 && hours < 17) {
-      timeOfDay = "Afternoon"
-    } else {
-      timeOfDay = "Night"
-    }
     return (
-        <h1>Good {timeOfDay}!</h1>
+      <div>
+        <Contact />
+      </div>
     );
 }
 
