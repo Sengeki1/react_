016ae58a35d83e0775766a3e9937c90db34b62cc
Sengeki1 <gamersengeki@gmail.com>
2024-03-10 12:59:06 -0100

    props properties

--- props/src/App.js
+++ props/src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
     return (
       <div>
-        <Contact />
+        <Contact 
+          img="./Images/Cat.jpg"
+          name="Mr. Whiskerson"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+        />
+        <Contact 
+          img="./Images/Cat_2.jpg"
+          name="Fluffykins"
+          phone="(212) 555-2345"
+          email="fluff@me.com" 
+        />
       </div>
     );
 }
