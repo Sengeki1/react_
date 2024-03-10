cab47e694dff2e1db51ba0915703095120f78593
Sengeki1 <gamersengeki@gmail.com>
2024-03-10 22:27:53 -0100

    conditional statement

--- airbnb_clone/src/Components/Card/Card.jsx
+++ airbnb_clone/src/Components/Card/Card.jsx
@@ -4,7 +4,7 @@ export default function Card (props) {
     return (
         <div className='card'>
             <div className='profile'>
-                <p>SOLD  OUT</p>
+                {props.openSpots === 0 && <p>SOLD  OUT</p>}
                 <img src={`./Images/Card/${props.img}`} className="photo"/><br/>
             </div>
             <div className='details'>
