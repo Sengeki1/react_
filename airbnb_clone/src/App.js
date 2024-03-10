eb55aed72058a79648f590ddd578928230a1ff8d
Sengeki1 <gamersengeki@gmail.com>
2024-03-10 21:46:03 -0100

    mapping through each item in an object

--- airbnb_clone/src/App.js
+++ airbnb_clone/src/App.js
@@ -1,21 +1,29 @@
 import NavBar from './Components/Header/NavBar'
 import Hero from './Components/Body/Hero'
 import Card from './Components/Card/Card'
+import data from './data.js'
 import './App.css';
 
 function App() {
+  
+  const cards = data.map(item => { 
+    return (
+      <Card 
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location = {item.location}
+        title = {item.title}
+        price = {item.price}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <NavBar />
       <Hero />
-      <Card 
-        img='image 12.png'
-        rating="5.0 "
-        reviewCount="(6)"
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price="From $136"
-      />
+      {cards} 
     </div>
   );
 }
--- airbnb_clone/src/Components/Card/Card.jsx
+++ airbnb_clone/src/Components/Card/Card.jsx
@@ -9,10 +9,10 @@ export default function Card (props) {
             </div>
             <div className='details'>
                 <img src='./Images/Card/Star 1.png'className="icon"/>
-                <a>{props.rating}<span>{props.reviewCount} - {props.country}</span></a>
+                <a>{props.rating}<span> ({props.reviewCount}) - {props.location}</span></a>
             </div>
             <p className='text-1'>{props.title}</p>
-            <p className='text-2'><b>{props.price}</b> / person</p>
+            <p className='text-2'><b>From ${props.price}</b> / person</p>
         </div>
     )
 }
