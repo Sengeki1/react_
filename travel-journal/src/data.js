faafc580dc1ff94d3097fe4ce9e1b12f16b04061
Sengeki1 <gamersengeki@gmail.com>
2024-03-11 15:30:01 -0100

    data

---
+++ travel-journal/src/data.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        title: "Alto Mira",
+        location: "Cabo Verde - Santo Antão",
+        googleMapsUrl: "https://www.google.com/maps/place/Ilha+de+Santo+Ant%C3%A3o/@17.0552652,-25.1621222,11z/data=!3m1!4b1!4m6!3m5!1s0x94f09355d883ac3:0x623c4ae19f70e48!8m2!3d17.091237!4d-25.141484!16zL20vMDVqNTBn?entry=ttu",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The island of Santo Antão is a place of unparalleled beauty. Located in the northwest part of Cape Verde, from the moment you set foot on the island, the contrast of the landscapes is unexpected and overwhelming.",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamy.com%2Fstock-photo-alto-mira-settlement-at-the-western-part-of-santo-antao-island-cap-177319730.html&psig=AOvVaw3YzTrzSQsri7DG3YljI93T&ust=1710260609700000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOD_9bLP7IQDFQAAAAAdAAAAABAV"
+
+    },
+    {
+        title: "São Pedro",
+        location: "Cabo Verde - São Vicente",
+        googleMapsUrl: "https://www.google.com/maps/place/S%C3%A3o+Pedro/data=!4m2!3m1!1s0x94f182df1b7e621:0xf578150eabc60e33?sa=X&ved=2ahUKEwiYoPz1z-yEAxVqhv0HHexzCu4Q8gF6BAgQEAA",
+        startDate: "14 Oct, 2023",
+        endDate: "14 Oct, 2023",
+        description: "São Pedro is a fishing village on the island of São Vicente, Cape Verde, belonging to the municipality of the same name and the parish of Nossa Senhora da Luz.",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Fgalvanol%2F8279075352&psig=AOvVaw3ASkyEco-6yqlcZXrcBRqa&ust=1710260832501000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCICCj53Q7IQDFQAAAAAdAAAAABAE"
+    }
+]
