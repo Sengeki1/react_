f173985d480e5afcfa04bec8d522db66dd98ee2b
Sengeki1 <gamersengeki@gmail.com>
2024-03-12 20:16:19 -0100

    finished journal

--- travel-journal/src/Components/Journal/journal.jsx
+++ travel-journal/src/Components/Journal/journal.jsx
@@ -1,17 +1,32 @@
 import './journal.css'
 
 export default function journal(props) {
+    const ColoredLine = ({ color }) => (
+        <hr
+            style={{
+                color: color,
+                backgroundColor: color,
+                height: 0/5
+            }}
+            className="hr"
+        />
+    );
     return (
-        <div className="journal">
-            <img src={`./Images/${props.imageUrl}`}/>
-            <div className="about-section">
-                <div className="location">
-                    <i class="fa fa-map-marker" aria-hidden="true"></i>
-                    <h4>{props.location}</h4>
-                    <a href={props.googleMapsUrl}>View on Google Maps</a>
+        <div>
+            <div className="journal">
+                <img src={`./Images/${props.imageUrl}`}/>
+                <div className="about-section">
+                    <div className="location">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        <h4>{props.location}</h4>
+                        <a href={props.googleMapsUrl}>View on Google Maps</a>
+                    </div>
+                    <h1>{props.title}</h1>
+                    <h5>{props.startDate} - {props.endDate}</h5>
+                    <p>{props.description}</p>
                 </div>
-                <h1>{props.title}</h1>
             </div>
+            <ColoredLine color="gray" />
         </div>
     )
 }
