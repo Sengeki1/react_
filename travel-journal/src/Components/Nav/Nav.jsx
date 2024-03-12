790751044e32a723029cfb2a7b6fe446cb4bc804
Sengeki1 <gamersengeki@gmail.com>
2024-03-12 11:54:08 -0100

    added navbar

--- travel-journal/src/Components/Nav/Nav.jsx
+++ travel-journal/src/Components/Nav/Nav.jsx
@@ -2,8 +2,9 @@ import './Nav.css'
 
 export default function Nav () {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <i class="fa fa-globe"></i>
+            <h2>my travel journal</h2>
         </div>
     )
 }
