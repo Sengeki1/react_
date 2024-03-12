6afbec7311abfa6010140a11162117882ca1c45d
Sengeki1 <gamersengeki@gmail.com>
2024-03-12 12:48:49 -0100

    location section

--- travel-journal/src/Components/Journal/journal.jsx
+++ travel-journal/src/Components/Journal/journal.jsx
@@ -2,6 +2,16 @@ import './journal.css'
 
 export default function journal(props) {
     return (
-        <div></div>
+        <div className="journal">
+            <img src={`./Images/${props.imageUrl}`}/>
+            <div className="about-section">
+                <div className="location">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    <h4>{props.location}</h4>
+                    <a href={props.googleMapsUrl}>View on Google Maps</a>
+                </div>
+                <h1>{props.title}</h1>
+            </div>
+        </div>
     )
 }
