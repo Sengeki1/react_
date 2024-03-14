de6770fbfb9daf3053d330efdffc06cd68c7479b
Sengeki1 <gamersengeki@gmail.com>
2024-03-14 19:48:47 -0100

    Form

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -1,10 +1,12 @@
 import Nav from './Components/NavBar/Nav.jsx'
+import Form from './Components/Form/Form.jsx'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Form />
     </div>
   );
 }
---
+++ meme_generator/src/Components/Form/Form.jsx
@@ -0,0 +1,13 @@
+import './Form.css'
+
+export default function Form () {
+    return (
+        <main>
+            <form className='form'>
+                <input className="form-input" placeholder='Top text' type="text"/>
+                <input className="form-input" placeholder='Bottom text' type="text"/>
+                <button className='form-button'>Get a new meme image</button>
+            </form>
+        </main>
+    )
+}
