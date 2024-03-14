460c1f4ef17f8758935aa5eb1e4fcc05cd58c109
Sengeki1 <gamersengeki@gmail.com>
2024-03-14 12:33:43 -0100

    navbar

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -1,9 +1,10 @@
+import Nav from './Components/NavBar/Nav.jsx'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
     </div>
   );
 }
---
+++ meme_generator/src/Components/NavBar/Nav.jsx
@@ -0,0 +1,15 @@
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div>
+            <div className='nav-section'>
+                <img src="./Images/Troll Face.png"/>
+                <h1>Meme Generator</h1>
+                <h3>React Course - Project 3</h3> 
+            </div>
+        </div>
+    )
+}
+
+export default Nav
