30901e57f8e024470d8d62b13ec52b979e4a95d4
Sengeki1 <gamersengeki@gmail.com>
2024-03-15 12:00:02 -0100

    data extracted from the API request

--- meme_generator/src/Components/Form/Form.jsx
+++ meme_generator/src/Components/Form/Form.jsx
@@ -1,20 +1,13 @@
 import './Form.css'
 
 export default function Form () {
-    function handleClick() {
-        console.log("I was clicked!")
-    }
-
-    function handleMouseOver () {
-        console.log("Mouse Over")
-    }
 
     return (
         <main>
             <form className='form'>
-                <input className="form-input" placeholder='Top text' type="text" onMouseOver={handleMouseOver}/>
+                <input className="form-input" placeholder='Top text' type="text"/>
                 <input className="form-input" placeholder='Bottom text' type="text"/>
-                <button className='form-button' onClick={handleClick}>Get a new meme image</button>
+                <button className='form-button'>Get a new meme image</button>
             </form>
         </main>
     )
