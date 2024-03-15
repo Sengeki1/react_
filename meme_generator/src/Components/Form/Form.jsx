22000976ddee0aa5e6cac04a033607953ec358b1
Sengeki1 <gamersengeki@gmail.com>
2024-03-15 12:23:30 -0100

    object distructuring

--- meme_generator/src/Components/Form/Form.jsx
+++ meme_generator/src/Components/Form/Form.jsx
@@ -1,13 +1,29 @@
 import './Form.css'
+import memesData from "../../memeData.js"
 
 export default function Form () {
+
+    function pickRandomProperty() {
+        const meme = memesData.data.memes 
+        const randomNumber = Math.floor(Math.random() * meme.length)
+        const { url } = meme[randomNumber] // Object distructuring, gets the item property from the array
+        console.log(url)
+    }
 
     return (
         <main>
             <form className='form'>
-                <input className="form-input" placeholder='Top text' type="text"/>
-                <input className="form-input" placeholder='Bottom text' type="text"/>
-                <button className='form-button'>Get a new meme image</button>
+                <input 
+                    className="form-input" 
+                    placeholder='Top text' 
+                    type="text"
+                />
+                <input 
+                    className="form-input" 
+                    placeholder='Bottom text' 
+                    type="text"
+                />
+                <button className='form-button' onClick={pickRandomProperty}>Get a new meme image</button>
             </form>
         </main>
     )
