9df780d3493b245c5d7abbfa4cfd49961e14a795
Sengeki1 <gamersengeki@gmail.com>
2024-03-16 09:40:25 -0100

    useState

--- meme_generator/src/Components/Form/Form.jsx
+++ meme_generator/src/Components/Form/Form.jsx
@@ -1,18 +1,22 @@
 import './Form.css'
 import memesData from "../../memeData.js"
+import React from "react"
 
 export default function Form () {
 
+    const [memeImage, setMemeImage] = React.useState(" ")
+    // useState is a method which allows to change the value of a property it receives a value and a function
     function pickRandomProperty() {
         const meme = memesData.data.memes 
         const randomNumber = Math.floor(Math.random() * meme.length)
         const { url } = meme[randomNumber] // Object distructuring, gets the item property from the array
-        console.log(url)
+        
+        setMemeImage(url)
     }
 
     return (
         <main>
-            <form className='form'>
+            <div className='form'>
                 <input 
                     className="form-input" 
                     placeholder='Top text' 
@@ -24,7 +28,8 @@ export default function Form () {
                     type="text"
                 />
                 <button className='form-button' onClick={pickRandomProperty}>Get a new meme image</button>
-            </form>
+            </div>
+            <img src={memeImage} className='meme-image'/>
         </main>
     )
 }
