c498ccdbb081cf394811d18b6b90d43000192da8
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-03-22 22:26:20 -0100

    toggle function implementation

--- Dinamic Styles/App.js
+++ Dinamic Styles/App.js
@@ -6,7 +6,16 @@ export default function App(props) {
     const [squares, setSquares] = React.useState(boxes)
     
     function toggle(id) {
-        
+        setSquares(squares.map(item => {
+            if (id == item.id) {
+                return {
+                    ...item,
+                    on: !item.on
+                }
+            } else {
+                return {...item}
+            }
+        }))
     }
 
     const squareElements = squares.map(square => (
